ab5c44d4e06d2240ba6b2b099b581f3babca35a9
Luke Masterson <lukemasterson@Lukes-MacBook-Air.local>
2024-03-21 21:21:45 +0000

    tested for and while loops

--- practice.js
+++ practice.js
@@ -405,4 +405,15 @@
 
 // console.log(calcAverage(totals));
 
+// Using a for loop
+
+// let nums = [1, 2, 3, 4, 5];
+// let results = [];
+// for (let num of nums) {
+//   results.push(num * 2);
+// }
+
+// console.log(results);
+
+
 
